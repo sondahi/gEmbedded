7da25bed556b3835bfa08baf1805afb32cd4848b
sondahi <gokay.comert@icloud.com>
2022-05-09 23:02:28 +0200

    test suite structure

--- gEmbedded-driver/src/main/c/native/bcm2711/test/main.c
+++ gEmbedded-driver/src/main/c/native/bcm2711/test/main.c
@@ -18,7 +18,10 @@ int main() {
 
     CU_ErrorCode error = CU_register_suites (suites);
 
-    printf ("Error code : %d",error);
+    if(error != CUE_SUCCESS){
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
